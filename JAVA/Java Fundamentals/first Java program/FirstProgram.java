19141d6cc7e28cd618691f2a2599bc4550a81d56
m0H-alessa <96932758+m0H-alessa@users.noreply.github.com>
2022-03-04 19:39:09 +0200

    indent

--- JAVA/Java Fundamentals/first Java program/FirstProgram.java
+++ JAVA/Java Fundamentals/first Java program/FirstProgram.java
@@ -1,7 +1,7 @@
 public class FirstProgram {
-    public static void main(String[] args){
-    System.out.println("My name is coding dojo");
-    System.out.println("I am 100 years old");
-    System.out.println("My hometown is Burbank, CA");
-}
+    public static void main(String[] args) {
+        System.out.println("My name is coding dojo");
+        System.out.println("I am 100 years old");
+        System.out.println("My hometown is Burbank, CA");
+    }
 }
