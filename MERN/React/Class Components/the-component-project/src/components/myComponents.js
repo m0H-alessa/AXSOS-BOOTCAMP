88023deb4f0fd78041b37e0105c62b4fa66c0810
m0H-alessa <96932758+m0H-alessa@users.noreply.github.com>
2022-04-11 05:56:05 +0300

    creating component project

---
+++ MERN/React/Class Components/the-component-project/src/components/myComponents.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+    
+    
+class MyComponent extends Component {
+    render() {
+        return <div>
+        <div className="box">
+        <h2>Mahmoud Alessa</h2>
+        <p>age: 33 </p>
+        <p>hair color: black-grey </p>
+        </div> 
+
+        <div className="box">
+        <h2>JOC</h2>
+        <p>age: 43 </p>
+        <p>hair color: bold no hair </p>
+        </div> 
+
+        <div className="box">
+        <h2>AVB</h2>
+        <p>age: 48 </p>
+        <p>hair color: brown </p>
+        </div> 
+
+        <div className="box">
+        <h2>Jehad Alessa</h2>
+        <p>age: 28 </p>
+        <p>hair color: black </p>
+        </div> 
+
+    
+    </div>;
+    }
+}
+    
+export default MyComponent;
